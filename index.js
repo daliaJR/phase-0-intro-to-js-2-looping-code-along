e90911983e218cb6bd9aa37490b9fc8f36142c3d
Dalia Jaber <daliajaber96@gmail.com>
2022-11-02 14:15:16 +0200

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arr,msg){
+    let output=[];
+    for(let i = 0; i<arr.length;i++){
+        output.push(`Thank you, ${arr[i]}, for the wonderful ${msg} gift!`);
+    }
+    return output;
+}
+
+writeCards(["Guadalupe", "Ollie", "Aki"],"surprise");
+
+function countDown(num){
+    for(let i = num ; i >= 0 ;i--){
+        
+        console.log(i);
+    }
+}
+console.log(countDown(10));
